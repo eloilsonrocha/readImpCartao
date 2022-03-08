200b5d28da1d841da2a891995a7d9ed9eaf825b8
eloilsondosanjos <eloilson.ageedu@gmail.com>
2022-03-07 22:47:05 -0300

    refactor: restructures input object into descriptors

--- src/routes.js
+++ src/routes.js
@@ -165,7 +165,7 @@ router.post("/descriptors", multerConfig.single("file"), async (request, respons
     descriptors.push({
       IdDescritor: descriptorLineSplit[0],
       CodigoDescritor: descriptorLineSplit[1],
-      DescricaoDescritor: descriptorLineSplit[2].trim(),
+      DescricaoDescritor: descriptorLineSplit[2].trim().toLowerCase(),
       AnoCursado: descriptorLineSplit[3],
       DataInclusao: descriptorLineSplit[4],
       Ativo: descriptorLineSplit[5],
@@ -173,14 +173,13 @@ router.post("/descriptors", multerConfig.single("file"), async (request, respons
     });
   }
 
-
   const descriptorsFull = [];
   const groupedResultDescriptors = new Map();
 
   for (let i = 1; i < descriptors.length; i += 1) {
     const howManyDescriptor = descriptors.filter((
-      descriptor) => descriptor.DescricaoDescritor === descriptors[i].DescricaoDescritor &&
-      descriptor.AnoCursado === descriptors[i].AnoCursado
+      descriptor) => descriptor.DescricaoDescritor == descriptors[i].DescricaoDescritor &&
+      descriptor.AnoCursado == descriptors[i].AnoCursado
     );
 
     const resultDescriptors = {
