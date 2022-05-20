cfa89f6524a2265a12ff0a7486b19faf86227c77
eloilsondosanjos <eloilson.ageedu@gmail.com>
2022-05-20 17:06:37 -0300

    feat: create html tag template

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require('cors')
 const router = require("./routes.js");
 
 const app = express()
+
+app.use(cors())
+
 app.use(router)
 
 app.listen(3000, () => console.log("Server is running on http://localhost:3000"))
